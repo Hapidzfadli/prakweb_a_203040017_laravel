429e436f7fec7958d4ae0715348947be97886d1e
hapidzfadli <hapidzfadli@gmail.com>
2022-10-17 15:05:55 +0700

    Video 3 Struktur Folder, Routes & View

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WPU BLOG | About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3>@php
+        echo $name;
+    @endphp</h3>
+    <p>@php
+        echo $email;
+    @endphp</p>
+    <img src="img/@php
+        echo $image;
+    @endphp" alt="">
+    <script src="js/script.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Hapid Fadli",
+        "email" => "hapidzfadli@gmail.com",
+        "image" => "hapidzfadli.jpg"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('blog');
 });
