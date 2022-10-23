41745211f3dced08a9634c17b1252127f2095f63
hapidzfadli <hapidzfadli@gmail.com>
2022-10-23 19:51:11 +0700

    Belajar Video #5 - Model, Collections & Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,55 +31,7 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Pertama",
-            "author" => "Hapid Fadli",
-            "slug" => "judul-post-pertama",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt laborum voluptatibus eius repudiandae, nostrum nulla magnam culpa dignissimos ex itaque corrupti provident rem odio id vitae consectetur officiis porro sint magni? Dolore similique soluta cupiditate ut sint maiores consectetur minus maxime tempore perferendis ea ex aspernatur eos excepturi illum id tenetur, corrupti laborum ratione quibusdam. Optio illo labore atque est voluptatibus expedita minus cum distinctio molestiae, at, ipsum consequatur quasi ea repudiandae facere possimus dolorem eaque ipsa fugit fugiat nulla. Eaque aperiam maxime, cum rem culpa itaque similique magnam quod, magni sit possimus provident quas amet, eum eveniet suscipit omnis."
-        ],
-
-        [
-            "title" => "Judul Post Kedua",
-            "author" => "Hapid Fadli",
-            "slug" => "judul-post-kedua",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt laborum voluptatibus eius repudiandae, nostrum nulla magnam culpa dignissimos ex itaque corrupti provident rem odio id vitae consectetur officiis porro sint magni? Dolore similique soluta cupiditate ut sint maiores consectetur minus maxime tempore perferendis ea ex aspernatur."
-        ],
-    ];
-    return view('posts', [
-        'title' => 'Blog',
-        'posts' => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 // halaman single post
-Route::get('posts/{slug}', function ($slug) {
-
-    $blog_posts = [
-        [
-            "title" => "Judul Pertama",
-            "author" => "Hapid Fadli",
-            "slug" => "judul-post-pertama",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt laborum voluptatibus eius repudiandae, nostrum nulla magnam culpa dignissimos ex itaque corrupti provident rem odio id vitae consectetur officiis porro sint magni? Dolore similique soluta cupiditate ut sint maiores consectetur minus maxime tempore perferendis ea ex aspernatur eos excepturi illum id tenetur, corrupti laborum ratione quibusdam. Optio illo labore atque est voluptatibus expedita minus cum distinctio molestiae, at, ipsum consequatur quasi ea repudiandae facere possimus dolorem eaque ipsa fugit fugiat nulla. Eaque aperiam maxime, cum rem culpa itaque similique magnam quod, magni sit possimus provident quas amet, eum eveniet suscipit omnis."
-        ],
-
-        [
-            "title" => "Judul Post Kedua",
-            "author" => "Hapid Fadli",
-            "slug" => "judul-post-kedua",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt laborum voluptatibus eius repudiandae, nostrum nulla magnam culpa dignissimos ex itaque corrupti provident rem odio id vitae consectetur officiis porro sint magni? Dolore similique soluta cupiditate ut sint maiores consectetur minus maxime tempore perferendis ea ex aspernatur."
-        ],
-    ];
-
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if ($post['slug'] === $slug) {
-            $new_post = $post;
-        }
-    }
-    return view('post', [
-        'title' => 'Single Post',
-        'post' => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
