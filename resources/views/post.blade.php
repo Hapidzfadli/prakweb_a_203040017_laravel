a2bd617aac25f37bb51cac890daece7613a1574f
hapidzfadli <hapidzfadli@gmail.com>
2022-10-25 22:45:36 +0700

    Belajar Video #9 - Database Seeder

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,8 +4,8 @@
 <article>
     <h2>{{$post->title}}</h2>
 
-    <p>By. Hapid Fadli in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
+    <p>By. <a class="text-decoration-none" href="#">{{$post->user->name}}</a> in <a class="text-decoration-none" href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
     <p>{!! $post->body !!}</p>
-    <a href="/blog">Back to posts</a>
+    <a href="/blog" class="d-block mt-3">Back to posts</a>
 </article>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,11 +3,14 @@
 @section('container')
 
 @foreach ($posts as $post)
-    <article class="mb-5">
+    <article class="mb-5 border-bottom">
         <h1>
-           <a href="/posts/{{$post->slug}}"> {{$post->title}}</a>
+           <a class="text-decoration-none" href="/posts/{{$post->slug}}">{{$post->title}}</a>
         </h1>
+        <p>By. <a class="text-decoration-none href="#">{{$post->user->name}}</a> in <a class="text-decoration-none" href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
         <p>{{$post->excerpt}}</p>
+
+        <a class="text-decoration-none pb-4" href="/posts/{{$post->slug}}">Read More..</a>
     </article>
 @endforeach
 <h2></h2>
