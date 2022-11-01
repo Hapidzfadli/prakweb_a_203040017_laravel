aedae3ba66df1e9cf6892a6d08a6c2919e8eed80
hapidzfadli <hapidzfadli@gmail.com>
2022-11-01 20:56:32 +0700

    Video #15 - User Registration

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,11 +18,17 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+
+    // protected $fillable = [
+    //     'name',
+    //     'username',
+    //     'email',
+    //     'password',
+    // ];
+
+    protected $guarded = ['id'];
+
+
 
     /**
      * The attributes that should be hidden for serialization.
