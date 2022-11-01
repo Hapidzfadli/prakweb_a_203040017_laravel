851cfe0222a1a03ce9f6392e668b07770b905600
hapidzfadli <hapidzfadli@gmail.com>
2022-11-01 21:30:19 +0700

    Video #16 - User Login & Middleware

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -20,9 +20,26 @@
           </li>
         </ul>
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a href="/login" class="nav-link {{($active === "login") ? 'active' : ''}}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
-          </li>
+            @auth
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Welcome back, {{auth()->user()->name}}
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#"><i class="bi bi-layout-text-sidebar-reverse"></i>My Dashboard</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                  <form action="/logout" method="post">
+                    @csrf
+                    <button type="submit" class="dropdown-item" ><i class="bi bi-box-arrow-right"></i>Logout</button>
+                  </form>
+              </ul>
+            </li>
+            @else
+            <li class="nav-item">
+              <a href="/login" class="nav-link {{($active === "login") ? 'active' : ''}}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+            </li>
+          @endauth
         </ul>
       </div>
     </div>
