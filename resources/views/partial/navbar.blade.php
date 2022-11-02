cb8ff843670572d9a412b9e664c004ffc8a491a2
hapidzfadli <hapidzfadli@gmail.com>
2022-11-02 21:27:22 +0700

    perbaiki navbar

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -26,12 +26,12 @@
                 Welcome back, {{auth()->user()->name}}
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#"><i class="bi bi-layout-text-sidebar-reverse"></i>My Dashboard</a></li>
+                <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
                 <li><hr class="dropdown-divider"></li>
                 <li>
                   <form action="/logout" method="post">
                     @csrf
-                    <button type="submit" class="dropdown-item" ><i class="bi bi-box-arrow-right"></i>Logout</button>
+                    <button type="submit" class="dropdown-item" ><i class="bi bi-box-arrow-right"></i> Logout</button>
                   </form>
               </ul>
             </li>
